7efb08d5774cb910d5694242705c32f7495de7d0
rifkyachong <rifkymarcelio@gmail.com>
2022-03-17 23:48:00 +0700

    create dragable component

--- myfirstgame.js
+++ myfirstgame.js
@@ -1,29 +1,32 @@
-var myComponent;
+var myComponent, TextComponent;
 var upBtn, downBtn, leftBtn, rightBtn, moveBtns;
 
 const startGame = () => {
   gameArea.start();
-  myComponent = new Component(50, 50, 10, 10, "red");
+  myComponent = new Component(0, 0, 20, 20, "red");
   myComponent.render();
+  myTextComponent = new Text("12px monospace", "Testing", "black", 450, 290);
+  myTextComponent.render();
   // controllers
   upBtn = document.getElementById("up-btn");
   leftBtn = document.getElementById("left-btn");
   rightBtn = document.getElementById("right-btn");
   downBtn = document.getElementById("down-btn");
   moveBtns = document.querySelectorAll(".move-btn");
-
-  // for (let btn of moveBtns) {
-  //   btn.onclick = handleClick;
-  //   btn.onmouseup = handleMouseUp;
-  //   btn.onmousedown = handleMouseDown;
-  //   // console.log(btn);
-  //   // btn.onmouseup = (e) => {
-  //   //   console.log("onmouseup clicked");
-  //   //   myComponent.speedX = 0;
-  //   //   myComponent.speedY = 0;
-  //   // };
-  // }
 };
+
+function Text(font, text, style, x, y) {
+  this.font = font;
+  this.text = text;
+  this.style = style;
+  this.x = x;
+  this.y = y;
+  this.render = () => {
+    gameArea.context.font = this.font;
+    gameArea.context.fillStyle = this.style;
+    gameArea.context.fillText(this.text, this.x, this.y);
+  };
+}
 
 function Component(x, y, width, height, color) {
   this.x = x;
@@ -33,6 +36,9 @@ function Component(x, y, width, height, color) {
   this.color = color;
   this.speedX = 0;
   this.speedY = 0;
+  this.onDrag = false;
+  this.initialOffsetTop = 0;
+  this.initialOffsetLeft = 0;
   this.render = () => {
     gameArea.context.fillStyle = this.color;
     gameArea.context.fillRect(this.x, this.y, this.width, this.height);
@@ -41,30 +47,73 @@ function Component(x, y, width, height, color) {
     this.x += this.speedX;
     this.y += this.speedY;
   };
+  // this.onDrag = () => {
+  //   let onDrag = false;
+  //   let left = this.x;
+  //   let right = this.x + this.width;
+  //   let top = this.y;
+  //   let bottom = this.y + this.height;
+  //   if (gameArea.cursorPosX && gameArea.cursorPosY) {
+  //     if (gameArea.cursorPosX > left && gameArea.cursorPosX < right && gameArea.cursorPosY > top && gameArea.cursorPosY < bottom ) {
+  //       this.initialOffsetLeft = gameArea.cursorPosX - this.x;
+  //       this.initialOffsetTop = gameArea.cursorPosY - this.y;
+  //       onDrag = true;
+  //     }
+  //   }
+  //   return onDrag;
+  // }
 }
+
+const detectClick = () => {
+  let left = myComponent.x;
+  let right = myComponent.x + myComponent.width;
+  let top = myComponent.y;
+  let bottom = myComponent.y + myComponent.height;
+  if (
+    gameArea.cursorPosX > left &&
+    gameArea.cursorPosX < right &&
+    gameArea.cursorPosY > top &&
+    gameArea.cursorPosY < bottom
+  ) {
+    myComponent.initialOffsetLeft = gameArea.cursorPosX - myComponent.x;
+    myComponent.initialOffsetTop = gameArea.cursorPosY - myComponent.y;
+    myComponent.onDrag = true;
+  }
+};
 
 const rerenderGameArea = () => {
   gameArea.clear();
-  let sumSpeedX = 0;
-  let sumSpeedY = 0;
-  if (gameArea.keys && gameArea.keys["ArrowUp"]) {
-    sumSpeedY -= 1;
+  // let sumSpeedX = 0;
+  // let sumSpeedY = 0;
+  // if (gameArea.keys && gameArea.keys["ArrowUp"]) {
+  //   sumSpeedY -= 1;
+  // }
+  // if (gameArea.keys && gameArea.keys["ArrowDown"]) {
+  //   sumSpeedY += 1;
+  // }
+  // if (gameArea.keys && gameArea.keys["ArrowLeft"]) {
+  //   sumSpeedX -= 1;
+  // }
+  // if (gameArea.keys && gameArea.keys["ArrowRight"]) {
+  //   sumSpeedX += 1;
+  // }
+  // myComponent.speedX = sumSpeedX;
+  // myComponent.speedY = sumSpeedY;
+  // myComponent.newPos();
+
+  if (myComponent.onDrag) {
+    myComponent.x = gameArea.cursorPosX - myComponent.initialOffsetLeft;
+    myComponent.y = gameArea.cursorPosY - myComponent.initialOffsetTop;
   }
-  if (gameArea.keys && gameArea.keys["ArrowDown"]) {
-    sumSpeedY += 1;
-  }
-  if (gameArea.keys && gameArea.keys["ArrowLeft"]) {
-    sumSpeedX -= 1;
-  }
-  if (gameArea.keys && gameArea.keys["ArrowRight"]) {
-    sumSpeedX += 1;
-  }
-  myComponent.speedX = sumSpeedX;
-  myComponent.speedY = sumSpeedY;
-  myComponent.newPos();
+
   myComponent.render();
+  myTextComponent.render();
   //process before update
   window.requestAnimationFrame(rerenderGameArea);
+};
+
+const release = () => {
+  myComponent.onDrag = false;
 };
 
 const gameArea = {
@@ -77,7 +126,6 @@ const gameArea = {
 
     window.addEventListener("keydown", (e) => {
       this.keys = this.keys || [];
-      console.log(e.key);
       // console.log(this.keys);
       this.keys[e.key] = true;
     });
@@ -85,6 +133,61 @@ const gameArea = {
       this.keys[e.key] = false;
       // console.log(this.keys);
     });
+    this.canvas.addEventListener("mousemove", (e) => {
+      this.cursorPosX =
+        e.offsetX -
+        parseFloat(getComputedStyle(e.target).getPropertyValue("padding-left"));
+      this.cursorPosY =
+        e.offsetY -
+        parseFloat(getComputedStyle(e.target).getPropertyValue("padding-top"));
+      // console.log(`cursorX: ${this.cursorPosX}, cursorY: ${this.cursorPosY}`);
+    });
+    this.canvas.addEventListener("mousedown", (e) => {
+      detectClick();
+    });
+    this.canvas.addEventListener("mouseup", (e) => {
+      release();
+    });
+    // this.canvas.addEventListener("mousedown", (e) => {
+    //   console.log("onmousedown called");
+    //   if (e.which === 1) {
+    //     myComponent.initialOffsetTop =
+    //       e.offsetY -
+    //       parseFloat(
+    //         getComputedStyle(e.target).getPropertyValue("padding-top")
+    //       ) -
+    //       myComponent.y;
+    //     myComponent.initialOffsetLeft =
+    //       e.offsetX -
+    //       parseFloat(
+    //         getComputedStyle(e.target).getPropertyValue("padding-left")
+    //       ) -
+    //       myComponent.x;
+    //     myComponent.onDrag = true;
+    //   }
+    // });
+    // this.canvas.addEventListener("mouseup", (e) => {
+    //   if (e.which === 1) {
+    //     myComponent.onDrag = false;
+    //   }
+    // });
+    // this.canvas.addEventListener("mousemove", (e) => {
+    //   console.log(`x: ${e.pageX} , y: ${e.pageY}`);
+    //   if (myComponent.onDrag) {
+    //     myComponent.y =
+    //       e.offsetY -
+    //       parseFloat(
+    //         getComputedStyle(e.target).getPropertyValue("padding-top")
+    //       ) -
+    //       myComponent.initialOffsetTop;
+    //     myComponent.x =
+    //       e.offsetX -
+    //       parseFloat(
+    //         getComputedStyle(e.target).getPropertyValue("padding-left")
+    //       ) -
+    //       myComponent.initialOffsetLeft;
+    //   }
+    // });
   },
   clear: function () {
     if (this.context) {
